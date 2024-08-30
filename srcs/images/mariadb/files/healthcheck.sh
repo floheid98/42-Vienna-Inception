fd8a0a8efbd72da4af63126896858f064ec090cc
floheid98 <florian.heid98@googlemail.com>
2024-08-30 16:38:26 +0200

    FIX: removed debug messages and unused code

--- srcs/images/mariadb/files/healthcheck.sh
+++ srcs/images/mariadb/files/healthcheck.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-echo "STARTING HEALTHCHECK" >&2
-
 init()
 {
-# mysql -u root << EOF
-# CREATE DATABASE IF NOT EXISTS wp_database;
-# USE wp_database;
-# CREATE USER IF NOT EXISTS 'wpuser'@'%' IDENTIFIED BY 'password';
-# GRANT ALL PRIVILEGES ON wp_database.* TO 'wpuser'@'%';
-# FLUSH PRIVILEGES;
-# EOF
-
 mysql -u root << EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 USE $DB_NAME;
@@ -24,14 +14,9 @@ EOF
 check()
 {
 if mysqladmin ping --silent; then
-    echo "STARTING SETUP SCRIPT" >&2
-
     init
-
-    echo "YEEEEEEEEEES" >&2
     exit 0
 else
-    echo "NEIIIIIIIIIIIIN" >&2
     exit 1
 fi
 }
