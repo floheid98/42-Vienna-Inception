09364d17b9bff6bc3a50f71ae6dd4ab1adb488dd
floheid98 <florian.heid98@googlemail.com>
2024-08-31 15:35:13 +0200

    FIX: modified files for ftp container, now working

--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+echo "$FTP_USER" | tee -a /etc/vsftpd.userlist
+
 adduser $FTP_USER --disabled-password
+
+echo "$FTP_USER:$FTP_PASSWORD" | /usr/sbin/chpasswd
+
+mkdir -p /var/www/html/ftp
+mkdir -p /var/run/vsftpd/empty
+
+chown $FTP_USER:$FTP_USER /var/www/html/ftp
+
+if (/usr/sbin/vsftpd) then
+    echo "ALAAAAAAAAAAAAAARM"
+fi
